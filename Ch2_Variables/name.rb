85f0527305489b4aae9c53e4f425753204ba8d5a
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-06 21:50:40 +0000

    Add Exercise 3 update to name.rb

--- Ch2_Variables/name.rb
+++ Ch2_Variables/name.rb
@@ -1,4 +1,9 @@
 print "Please type in your name: "
 name = gets.chomp
 
-puts "Hi there, #{name}!"
+puts "Hi there, #{name}."
+
+# You don't have to have the block parameter if you don't want
+10.times do
+  puts name
+end
