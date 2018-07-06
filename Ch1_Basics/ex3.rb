25f4319e35c81e5d21940f2e0fce7fd44aa90976
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-06 17:52:47 +0000

    Add completed Exercise 3

---
+++ Ch1_Basics/ex3.rb
@@ -0,0 +1,11 @@
+movies = {
+  rampage: 2018,
+  home_alone: 1990,
+  home_alone2: 1992,
+  toy_story: 1995
+}
+
+puts movies[:rampage]
+puts movies[:home_alone]
+puts movies[:home_alone2]
+puts movies[:toy_story]
