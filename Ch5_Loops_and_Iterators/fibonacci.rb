7d166072d35c3908a76939170cbe2f0843d595e0
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-09 22:56:20 +0000

    Add more comments

--- Ch5_Loops_and_Iterators/fibonacci.rb
+++ Ch5_Loops_and_Iterators/fibonacci.rb
@@ -1,8 +1,12 @@
+# The key concept with recursion is that there is a base case that returns a
+# value, which then "unwinds" the recursive calls.
+
 # Code to determine the nth number in the Fibonacci sequence (that starts at 1)
 def fibonacci(number)
   if number < 2
     number
   else
+    # 2 recursive calls in the recursive case
     fibonacci(number - 1) + fibonacci(number - 2)
   end
 end
