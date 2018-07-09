a849ab54d5d299d19226de796ecd181e6f4c9f5d
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-09 22:15:23 +0000

    Add fibonacci.rb

---
+++ Ch5_Loops_and_Iterators/fibonacci.rb
@@ -0,0 +1,12 @@
+# Code to determine the nth number in a Fibonacci sequence (that starts at 1)
+def fibonacci(number)
+  if number < 2
+    number
+  else
+    fibonacci(number - 1) + fibonacci(number - 2)
+  end
+end
+
+# What is the 10th number in the Fibonacci sequence?
+puts fibonacci(10)
+# => 55
