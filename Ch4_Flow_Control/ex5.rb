bfa348680b7bd85eaee72b4ce4002275fc35cad3
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-09 18:48:03 +0000

    Add completed Exercise 5

---
+++ Ch4_Flow_Control/ex5.rb
@@ -0,0 +1,17 @@
+print "Please enter a number from 0 to 100: "
+num = gets.chomp.to_i
+
+def number_test(num)
+  case
+  when num < 0
+    "less than 0"
+  when num <= 50
+    "between 0 and 50"
+  when num <= 100
+    "between 51 and 100"
+  else
+    "greater than 100"
+  end
+end
+
+puts "Your number is #{number_test(num)}."
