6d060f00cbbb513b6711579ad2714e99b1cf0d58
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-11 23:11:20 +0000

    Add completed Exercise 8

---
+++ Exercises/ex8.rb
@@ -0,0 +1,15 @@
+pet_count = {
+  :dogs => 2,
+  :cats => 4,
+  :goats => 3
+}
+
+miles_run = {
+  monday: 0,
+  tuesday: 4,
+  wednesday: 4,
+  thursday: 2,
+  friday: 3,
+  saturday: 5,
+  sunday: 10
+}
