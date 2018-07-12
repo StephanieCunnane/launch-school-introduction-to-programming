8e5a61f860e91b52ae5f36c9195c94adf7d63ddf
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2018-07-12 00:04:26 +0000

    Add completed Exercise 10

---
+++ Exercises/ex10.rb
@@ -0,0 +1,11 @@
+# Examples of nested collections
+
+# Arrays can be hash values
+wardrobe = {
+  shirts: ['red', 'blue', 'fancy', 'green'],
+  pants: ['dress', 'work', 'jeans', 'shorts'],
+  jackets: ['fall', 'sports', 'winter']
+}
+
+# Arrays can contain hashes
+team_members = [{name: "Stephanie", position: "pitcher"}, {name: "Natasha", position: "first base"}]
